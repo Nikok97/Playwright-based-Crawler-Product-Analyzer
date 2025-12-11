13fbcf791b0ba1668b52f8483bab55e20ec3672f
Nicolás Bellino <nicolasbellino97@gmail.com>
2025-12-10 23:01:36 -0300

    Update crawler.py

--- src/crawler.py
+++ src/crawler.py
@@ -6,18 +6,24 @@ from playwright.sync_api import sync_playwright
 from stealth import stealth_context, human_scroll
 from db import db_initialization, insert_url, now, update_url_date, update_filename_for_url
 from utils import setup_loggers, setup_directories
+from specific_sites import AmazonConfig
 
 #Config setup and directories
 BASE_DIR, CURRENT_DIR, PARENT_DIR, DATA_DIR = setup_directories()
 
 #Load config
 config_path = os.path.join(BASE_DIR, "config.json")
+
 with open(config_path) as f:
     config = json.load(f)
-    config_db_path = config.get("database_path", "mini.sqlite")
-    seed_url = config['seed_url']
+    config_db_path = config.get("database_path", "mini.sqlite") 
     start_position = config["start_position"]
     end_position = config["end_position"]
+
+#Specific site config
+specific_site_config = AmazonConfig()
+seed_url = specific_site_config.seed_urls[0]
+
 #DB setup
 db_path = os.path.join(DATA_DIR, config_db_path)
 db = db_initialization(db_path)
@@ -28,7 +34,7 @@ logger, error_logger = setup_loggers()
 #Pagination
 list_of_urls = list()
 for i in range(start_position, end_position):
-    url = f"{seed_url}&page={i}"
+    url = specific_site_config.build_pagination_url(seed_url, i)
     list_of_urls.append(url)
 
 page_counter = 1
@@ -50,8 +56,8 @@ with sync_playwright() as p:
 
         try:
             page.goto(list_of_urls[i], timeout=30000)
-            page.wait_for_selector("span.a-price-whole", timeout=8000)
-            print('Selector detected')
+            page.wait_for_selector(specific_site_config.product_price_selector, timeout=8000)
+            print(f'Target JavaScript selector detected in URL: {list_of_urls[i]}')
         except Exception as e:
             logger.info(f"Retrying {list_of_urls[i]}: {e}")
             time.sleep(random.uniform(15, 25))
@@ -60,15 +66,15 @@ with sync_playwright() as p:
         try:
             time.sleep(random.uniform(11, 14))
             page.reload(timeout=30000)
-            page.wait_for_selector("span.a-price-whole", timeout=8000)
+            page.wait_for_selector(specific_site_config.product_price_selector, timeout=8000)
         except Exception as e:
             error_logger.warning(f"Second failure on {list_of_urls[i]}: {e}")
             continue
 
         #Scroll
-        human_scroll(page)
         print("Scrolling")
-
+        human_scroll(page)
+        
         time.sleep(random.uniform(1.0, 2.2))
 
         try:
@@ -93,15 +99,15 @@ with sync_playwright() as p:
                 try:
 
                     date = str(now())
-                    print(f"Attempting to insert URL: {list_of_urls[i]}")
+                    print(f"1. Attempting to insert URL: {list_of_urls[i]}")
                     insert_url(list_of_urls[i], db)
-                    print(f"Inserted URL: {list_of_urls[i]}")
-                    print(f"Attempting to update date of URL: {list_of_urls[i]}")
+                    print(f"2. Inserted URL: {list_of_urls[i]}")
+                    print(f"3. Attempting to update date of URL: {list_of_urls[i]}")
                     update_url_date(list_of_urls[i], db, date)
-                    print(f"Date updated for URL: {list_of_urls[i]}")
-                    print(f"Attempting to update filename of URL: {list_of_urls[i]}")
+                    print(f"4. Date updated for URL: {list_of_urls[i]}")
+                    print(f"5. Attempting to update filename of URL: {list_of_urls[i]}")
                     update_filename_for_url(list_of_urls[i], db, filename)
-                    print(f"Filename updated for URL: {list_of_urls[i]}")
+                    print(f"6. Filename updated for URL: {list_of_urls[i]}")
 
                 except Exception as e:
                     error_logger.warning(f"Unknown DB error for {list_of_urls[i]}: {e}")
@@ -109,7 +115,7 @@ with sync_playwright() as p:
             page_counter += 1
 
             print("Now waiting...")
-            time.sleep(random.uniform(39, 47))
+            time.sleep(random.uniform(30, 50))
 
         except Exception as e:
             error_logger.warning(f"Unknown error for {list_of_urls[i]}: {e}")
