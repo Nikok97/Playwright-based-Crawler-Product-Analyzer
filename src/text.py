8e4c6d0f19cc2038dd2c1a85c231d4e5d07785e8
Nicolás Bellino <nicolasbellino97@gmail.com>
2025-12-15 17:38:26 -0300

    Update text.py

--- src/text.py
+++ src/text.py
@@ -3,10 +3,10 @@ import os
 import json
 from db import db_initialization, insert_product, get_url_and_id_using_filename
 from utils import setup_loggers, setup_directories
-from classes_specific_sites import AmazonConfig
+from classes_specific_sites import AmazonConfig, MercadoLibreConfig
 
 #Specific sit config
-specific_site_config = AmazonConfig()
+specific_site_config = MercadoLibreConfig()
 
 #Config directories
 BASE_DIR, CURRENT_DIR, PARENT_DIR, DATA_DIR = setup_directories()
@@ -46,15 +46,17 @@ for file in os.listdir(DATA_DIR):
             #3. Containers, price, currency and name extraction
             
             products_of_page = specific_site_config.product_extraction(soup)
+            counter_of_products_per_page = 1
 
             #DB Product insertion
             for individual_product in products_of_page:
 
+                total_number_of_products_in_page = int(len(products_of_page))
 
                 #Go over the list products of page and insert the product with the corresponding URL_ID in the DB
-
                 insert_product(db, individual_product, url_id)
-                print(f"Inserted product for URL {url_id}")
+                print(f"Inserted product {counter_of_products_per_page} of {total_number_of_products_in_page} for URL {url_id}")
+                counter_of_products_per_page += 1
 
 db["conn"].close()
 
